a21ad31d54cf8305ad7ac5b3972c77a617987692
Affan Mansoor <affanmansoor630@gmail.com>
2023-06-01 22:12:24 +0500

    completed

---
+++ main.js
@@ -0,0 +1,21 @@
+const tab = document.getElementById('tab');
+const tableArea = document.getElementById('tableArea');
+const timeArea = document.getElementById('timeArea');
+
+function dispTable()
+{
+    let tabNum = tab.value;
+    tableArea.innerHTML = `<h4>Displaying table of ${tabNum}</h4>`;
+    for(let count = 1;count <= 10;count++)
+    {
+        tableArea.innerHTML +=
+        `<p>${tabNum} X ${count} = ${tabNum*count}</p>`;
+    }
+}
+
+function dispTime() {
+    let dt = new Date();
+    timeArea.innerHTML = `<span>${dt}</span>`;
+}
+
+setInterval(dispTime,1000);
